9002dfe9630f9252ffaf5a616202648daa59613a
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-09-06 00:16:43 +0530

    tried something on map

--- FRONTEND/src/Home-Page/Map.jsx
+++ FRONTEND/src/Home-Page/Map.jsx
@@ -17,7 +17,7 @@ L.Marker.prototype.options.icon = DefaultIcon;
 export function Map() {
   return (
     <div className="page-container">
-      <MapContainer center={[22.5490755, 88.3319375]} zoom={13} className="map-view">
+      <MapContainer center={[22.5490755, 88.3319375]} zoom={16} className="map-view">
         {/* Base map tiles */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
@@ -33,9 +33,9 @@ export function Map() {
 
         {/* Circle */}
         <Circle
-          center={[51.508, -0.11]}
-          radius={500}
-          pathOptions={{ color: "red", fillColor: "#f03", fillOpacity: 0.5 }}
+          center={[22.5490755, 88.3319375]}
+          radius={150}
+          pathOptions={{ color: "red", fillColor: "#f03", fillOpacity: 1 }}
         >
           <Popup>I am a circle.</Popup>
         </Circle>
