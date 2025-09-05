6399c7f0d4f346bc88441d779e0d8ac736edc927
Rajesh000001 <rkm74462@gmail.com>
2025-09-05 19:11:22 +0530

    Login Button Component Added

--- FRONTEND/src/Home-Page/Home.jsx
+++ FRONTEND/src/Home-Page/Home.jsx
@@ -1,25 +1,9 @@
 import { Map } from "./Map.jsx";
-import { useNavigate } from "react-router-dom";
-import "./Home-Page.css";
+import { LoginProfileButton } from "../Components/Login-profile-button.jsx";
 export function Home() {
-  const navigate = useNavigate();
-
-  const handleProfileClick = () => {
-    const user =
-      localStorage.getItem("username") || localStorage.getItem("email");
-    if (user) {
-      navigate("/profile");
-    } else {
-      navigate("/login");
-    }
-  };
   return (
     <>
-      <div id="login-profile">
-        <button id="login-profile-btn" onClick={handleProfileClick}>
-          Login/Profile
-        </button>
-      </div>
+      <LoginProfileButton />
       <Map />
     </>
   );
