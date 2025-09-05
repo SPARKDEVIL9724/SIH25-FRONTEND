3841b5a3254a5c7b5c3845f38b0b687a678874a5
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-09-05 11:35:05 +0530

    completed manage trains page-> data addition left

--- FRONTEND/src/Manage-Trains-Page/ManageTrains.jsx
+++ FRONTEND/src/Manage-Trains-Page/ManageTrains.jsx
@@ -5,16 +5,20 @@ function createData(trainID, source, destination, status, ETA, speed, currentSta
 };
 
 export function ManageTrains(){
+  //demo data
     const [trains, setTrains] = useState([
-        createData('T1', 'Alice Johnson', 'alice.j@example.com', 'Delayed', 'Tech Corp', '40kmh','Howrah'),
-        createData('T2', 'Bob Williams', 'bob.w@example.com', 'Halted', 'Innovate Ltd.', '400kmh','Howrah'),
-        createData('T3', 'Charlie Davis', 'charlie.d@example.com', 'Delayed', 'Creative Inc.', '40kmh', 'Howrah'),
-        createData('T4', 'Diana Rodriguez', 'diana.r@example.com', 'On Time', 'Global Solutions', '90kmh', 'Howrah'),
-    ]);
-    const [expandedRows, setExpandedRows] = useState([]);
-
-    //filtering function
-    function filterByDelayed(event){
+        createData('T1', 'Howrah Jn.', 'Dhanbad Jn.', 'Delayed', '7:00', '40kmh','Howrah'),
+        createData('T2', 'Asansol Jn.', 'Kharagpur Jn.', 'Halted', '12:00', '400kmh','Howrah'),
+        createData('T3', 'Howrah Jn.', 'Dhanbad Jn.', 'Delayed', '15:00', '40kmh', 'Howrah'),
+        createData('T5', 'Asansol Jn.', 'Kharagpur Jn.', 'Halted', '12:00', '400kmh','Howrah'),
+        createData('T4', 'Howrah Jn.', 'Asansol Jn.', 'On Time', '21:00', '90kmh', 'Howrah'),
+      ]);
+      const [expandedRows, setExpandedRows] = useState([]);
+      
+      const stations = ['Howrah Jn.', 'Dhanbad Jn.', 'Kharagpur Jn.', 'Asansol Jn.', 'DumDum Jn.'];
+      const operations = ['Start', 'Halt'];
+    //filtering functions
+    function filterByStatus(event){
         const filterStatus = event.target.value;
         // filtering on given status
         const filteredTrains = trains.filter((train) => {
@@ -29,6 +33,50 @@ export function ManageTrains(){
         const newTrainsList = [...filteredTrains , ...rest];
         setTrains(newTrainsList);
     }
+    function filterBySource(event){
+        const filterSource = event.target.value;
+        // filtering on given source
+        const filteredTrains = trains.filter((train) => {
+            return train.source === filterSource;
+        });
+        
+        const rest = trains.filter((train) => {
+            return train.source !== filterSource;
+        });
+        
+        //creating new List
+        const newTrainsList = [...filteredTrains , ...rest];
+        setTrains(newTrainsList);
+    }
+    function filterByDestination(event){
+        const filterDestination = event.target.value;
+        // filtering on given status
+        const filteredTrains = trains.filter((train) => {
+            return train.destination === filterDestination;
+        });
+        
+        const rest = trains.filter((train) => {
+            return train.destination !== filterDestination;
+        });
+        
+        //creating new List
+        const newTrainsList = [...filteredTrains , ...rest];
+        setTrains(newTrainsList);
+    }
+    function changeTrainStatus(event, tid){
+      const newTrainStatus = event.target.value;
+      const newTrains = trains.forEach((train) => {
+            if(train.trainID === tid){
+              if(newTrainStatus==='Start'){
+                train.status='Delayed';
+              }
+              if(newTrainStatus==='Halt'){
+                train.status='Halted';
+              }
+            }
+        });
+      setTrains([...newTrains]);
+    }
     //function toggle row
   const toggleRow = (id) => {
     setExpandedRows((prev) =>
@@ -40,14 +88,28 @@ export function ManageTrains(){
     <div className='container'>
         <h1>Manage Trains</h1>
         <div className='filter-container'>
-
-        <label>Filter:</label>
-        <select className='filter-by-status' name='filterByStatus' onChange={(e) => filterByDelayed(e)}>
+        <label>Filter By Status:</label>
+        <select className='filter-by-status' name='filterByStatus' onChange={(e) => filterByStatus(e)}>
+            <option value=''>Filter By Status</option>
             <option value="On Time">On TIme</option>
             <option value="Delayed">Delayed</option>
             <option value="Halted">Halted</option>
             <option value="Arrived">Arrived</option>
             <option value="Cancelled">Cancelled</option>
+        </select><br/>
+        </div>
+        <div className='filter-container'>
+        <label>Filter By Start:</label>
+        <select className='filter-by-source' name='filterBySource' onChange={(e) => filterBySource(e)}>
+            <option value=''>Start</option>
+            {stations.map((station) => <option value={station}>{station}</option>)}
+        </select>
+        </div>
+        <div className='filter-container'>
+        <label>Filter By Status:</label>
+        <select className='filter-by-destination' name='filterByDestination' onChange={(e) => filterByDestination(e)}>
+            <option value=''>Destination</option>
+            {stations.map((station) => <option value={station}>{station}</option>)}
         </select>
         </div>
     <table border="1" cellPadding="10" style={{ borderCollapse: 'collapse', width: '100%' }}>
@@ -82,6 +144,16 @@ export function ManageTrains(){
                 <td colSpan="4" style={{ backgroundColor: '#f9f9f9' }}>
                   Speed: {train.speed}<br/>
                   Current Station: {train.currentStation}
+                </td>
+                <td>
+                  <label>Change Train Status:</label>
+                  <select className='change-train-status' name='perfromOperation' onChange={(e) => changeTrainStatus(e, train.trainID)}>
+                    <option value=''>Operation</option>
+                    {operations.filter((operation) => {
+                      if(train.status === 'On Time' || train.status==='Delayed'){return operation!=='Start'}
+                      if(train.status==='Halted'){return operation!=='Halt'}
+                    }).map((operation) => <option value={operation}>{operation}</option>)}
+                  </select>
                 </td>
               </tr>
             )}
